0d18964b395e579c096d3175b6994bf3c029d2f8
dariomvg <contact.dario.info@gmail.com>
2024-03-01 18:32:05 -0300

    segundo commit

---
+++ src/utils/filterTrailer.js
@@ -0,0 +1,10 @@
+export const filterTrailer = (data) => {
+  if (data.videos.results) {
+    const trailer = data.videos.results.filter((video) =>
+      video.name === "Official Trailer"
+        ? video
+        : video.name === "Teaser Trailer"
+    );
+    return trailer;
+  }
+};
---
+++ src/utils/options.js
@@ -0,0 +1,19 @@
+export const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjczMmVmZDM0YzI1ZDRiNTQ5ZTZhN2UwMDRmZTcwMSIsInN1YiI6IjY0Yjg0OTRlNGQyM2RkMDEwNjk1Y2ZjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LwDNqmYOmsq4hFCbLSoFSJQoXm-f7Mu4qcRM_z5a3DA",
+  },
+};
+
+export const api_key = "3f732efd34c25d4b549e6a7e004fe701";
+
+export const api_url = "https://api.themoviedb.org/3";
+export const base_url = "https://api.themoviedb.org/3/search/movie";
+export const image_url = "https://image.tmdb.org/t/p/original";
+export const image_url_500 = "https://image.tmdb.org/t/p/w300";
+export const url_trailer = "https://www.youtube.com/watch?v=";
+export const url_series =
+  "https://api.themoviedb.org/3/trending/tv/day?language=en-US";
+export const url_popular = "https://api.themoviedb.org/3/movie/upcoming";
